5604e0bb15fa2cd06c4c57898cd28add1a5de1c6
Robert Simmons <Simmons.Robert@outlook.com>
2019-05-09 13:21:18 -0400

    Another quick commit

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,22 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    if(typeof(0) === typeof(someNum) && someNum !== Infinity){
+        let finalnum = 1
+        let someNum2 = Math.abs(someNum)
+        while(someNum2 > 1){
+            someNum2 /= 2
+            if(someNum2 === finalnum){
+                return true
+                break
+            }
+        }
+        return false
+    }else{
+        throw 'This isn\'t a workable number!'
+    }
+    
 }
+
+console.log(isEven(17))
+console.log(isEven(8))
--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,18 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    if(typeof('') === typeof(someStr)){
+        for(let key = 0; key < someStr.length; key++){
+            if(someStr.charAt(key) !== someStr.charAt(someStr.length-key-1)){
+                return false
+                break
+            }
+        }
+        return true
+    }else{
+        throw 'What is this???'
+    }
 }
+
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('win'))
