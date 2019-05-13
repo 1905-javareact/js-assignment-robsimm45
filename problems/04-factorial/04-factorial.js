b580025da026c7cc1f8becdcad1aacfac5c7eb55
Robert Simmons <Simmons.Robert@outlook.com>
2019-05-13 15:20:32 -0400

    Okay now finalized

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,24 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+  if(typeof(sumNum) !== typeof(0)){
+      return 'You didn\'t give me a number!'
+  }
+
+  if(sumNum < 1){
+      return 'Too low of a number!'
+  }
+
+  if(sumNum === Infinity){
+      return 'This is too much!'
+  }
+
+  if(sumNum === 1){
+      return 1
+  }
+
+  return (sumNum * factorial(sumNum-1))
+
 }
+
+console.log(factorial(170))
--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -22,4 +22,71 @@ Example for printShape("Diamond", 5, "*");
 */
 function printShape(shape, height, character) {
   
+  let charHold = ''
+  switch (shape){
+    case "Square":
+      for(let sh = 0; sh < height; sh++){
+        for(let sw = 0; sw < height; sw++){
+          charHold += character
+        }
+        console.log(charHold)
+        charHold = ''
+      }
+      break;
+    case "Triangle":
+      for(let th = 0; th < height; th++){
+        for(let tw = 0; tw <= th; tw++){
+          charHold += character
+        }
+        console.log(charHold)
+        charHold = ''
+      }
+      break;
+
+    case "Diamond":
+      for(let dh = 0; dh < height; dh++){
+        if(dh%2 === 0){
+          for(let begin = 0; begin < (height-dh)/2; begin++){
+            charHold += ' '
+          }
+
+          
+          for(let dw = 0; dw <= dh; dw++){
+            charHold += character
+          }
+          
+          charHold += ' '
+          console.log(charHold)
+          charHold = ''
+        }
+        
+      }
+
+      for(let dh = height; dh >= 0; dh--){
+        if(dh%2 === 0 && dh !== height-1){
+          for(let begin = 0; begin < (height-dh)/2; begin++){
+            charHold += ' '
+          }
+
+          
+          for(let dw = 0; dw <= dh; dw++){
+            charHold += character
+          }
+          
+          charHold += ' '
+          console.log(charHold)
+          charHold = ''
+        }
+        
+      }
+
+
+      break;
+
+    default:
+  }
 }
+
+printShape("Square", 5, '*')
+printShape("Triangle", 3, '-')
+printShape("Diamond", 7, '$')
